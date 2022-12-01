b54b2ff0fc86d91282c7c46321b444cef877ba11
Lee Spottiswood <me@lee.io>
2022-12-01 13:39:15 +0000

    fix log levels

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -9,19 +9,19 @@ func (l *ProviderLogger) Error(msg string) {
 }
 
 func (l *ProviderLogger) Warn(msg string) {
-	l.log("ERROR", msg)
+	l.log("WARN", msg)
 }
 
 func (l *ProviderLogger) Info(msg string) {
-	l.log("ERROR", msg)
+	l.log("INFO", msg)
 }
 
 func (l *ProviderLogger) Debug(msg string) {
-	l.log("ERROR", msg)
+	l.log("DEBUG", msg)
 }
 
 func (l *ProviderLogger) Trace(msg string) {
-	l.log("ERROR", msg)
+	l.log("TRACE", msg)
 }
 
 func (l *ProviderLogger) log(level string, msg string) {
