8f436d0a14ea1384662fc45e12c2d08526cbacb7
Dane Elwell <dane.elwell@ans.co.uk>
2023-11-13 11:09:31 +0000

    Add support for vCPU sockets/cores_per_socket

--- ecloud/resource_instance_test.go
+++ ecloud/resource_instance_test.go
@@ -1,8 +1,11 @@
 package ecloud
 
 import (
+	"bytes"
 	"fmt"
+	"regexp"
 	"testing"
+	"text/template"
 
 	ecloudservice "github.com/ans-group/sdk-go/pkg/service/ecloud"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
@@ -116,4 +119,153 @@ resource "ecloud_instance" "test-instance" {
 	vcpu_cores = 1
 }
 `, regionID, instanceName)
+}
+
+type vcpuTestConfig struct {
+	RegionID           string
+	Name               string
+	VCPUCores          int
+	VCPUSockets        int
+	VCPUCoresPerSocket int
+}
+
+// TestAccInstance_vcpu tests vcpu_cores / vcpu.sockets / vcpu.cores_per_socket options. May require test timeout
+// increasing with `-timeout 15m`.
+func TestAccInstance_vcpu(t *testing.T) {
+	instanceName := acctest.RandomWithPrefix("tftest")
+	resourceName := "ecloud_instance.test-instance-vcpu"
+	vpcResourceName := "ecloud_vpc.test-vpc"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckInstanceDestroy,
+		Steps: []resource.TestStep{
+			// 1. Create instance using vcpu_cores
+			{
+				Config: testAccResourceInstanceConfig_vcpu(vcpuTestConfig{ANS_TEST_VPC_REGION_ID, instanceName, 1, 0, 0}),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckInstanceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", instanceName),
+					resource.TestCheckResourceAttrPair(vpcResourceName, "id", resourceName, "vpc_id"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu_cores", "1"),
+					resource.TestCheckNoResourceAttr(resourceName, "vcpu.0.sockets"),
+					resource.TestCheckNoResourceAttr(resourceName, "vcpu.0.cores_per_socket"),
+				),
+			},
+			// 2. Increase with vcpu_cores
+			{
+				Config: testAccResourceInstanceConfig_vcpu(vcpuTestConfig{ANS_TEST_VPC_REGION_ID, instanceName, 2, 0, 0}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "vcpu_cores", "2"),
+					resource.TestCheckNoResourceAttr(resourceName, "vcpu.0.sockets"),
+					resource.TestCheckNoResourceAttr(resourceName, "vcpu.0.cores_per_socket"),
+				),
+			},
+			// 3. Decrease with vcpu_cores
+			{
+				Config: testAccResourceInstanceConfig_vcpu(vcpuTestConfig{ANS_TEST_VPC_REGION_ID, instanceName, 1, 0, 0}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "vcpu_cores", "1"),
+					resource.TestCheckNoResourceAttr(resourceName, "vcpu.0.sockets"),
+					resource.TestCheckNoResourceAttr(resourceName, "vcpu.0.cores_per_socket"),
+				),
+			},
+			// 4. Switch from vcpu_cores to using vcpu.sockets/vcpu.cores_per_socket.
+			{
+				Config: testAccResourceInstanceConfig_vcpu(vcpuTestConfig{ANS_TEST_VPC_REGION_ID, instanceName, 0, 1, 1}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.sockets", "1"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.cores_per_socket", "1"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu_cores", "0"),
+				),
+			},
+			// 5. Increase our CPU count with vcpu.sockets/vcpu.cores_per_socket
+			{
+				Config: testAccResourceInstanceConfig_vcpu(vcpuTestConfig{ANS_TEST_VPC_REGION_ID, instanceName, 0, 2, 2}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.sockets", "2"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.cores_per_socket", "2"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu_cores", "0"),
+				),
+			},
+			// 6. Decrease our CPU count with vcpu.sockets/vcpu.cores_per_socket
+			{
+				Config: testAccResourceInstanceConfig_vcpu(vcpuTestConfig{ANS_TEST_VPC_REGION_ID, instanceName, 0, 1, 2}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.sockets", "1"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.cores_per_socket", "2"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu_cores", "0"),
+				),
+			},
+			// 7. Attempt to increase our CPU count with vcpu_cores - this should error
+			{
+				Config:      testAccResourceInstanceConfig_vcpu(vcpuTestConfig{ANS_TEST_VPC_REGION_ID, instanceName, 2, 0, 0}),
+				ExpectError: regexp.MustCompile(`.*vcpu cores can't be updated.*`),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.sockets", "1"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu.0.cores_per_socket", "2"),
+					resource.TestCheckResourceAttr(resourceName, "vcpu_cores", "0"),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceInstanceConfig_vcpu(config vcpuTestConfig) string {
+	if config.VCPUCores > 0 && (config.VCPUSockets > 0 || config.VCPUCoresPerSocket > 0) {
+		panic("bad test config, VCPUCores exclusive with VCPUSockets/VCPUCoresPerSocket")
+	}
+
+	tmpl := template.Must(template.New("tf1").Parse(`
+resource "ecloud_vpc" "test-vpc" {
+	region_id = "{{.RegionID}}"
+	name = "test-vpc"
+}
+
+data "ecloud_image" "ubuntu2204" {
+	name = "Ubuntu Server 22.04 LTS"
+}
+
+data "ecloud_availability_zone" "test-az" {
+	name = "Manchester West"
+}
+
+resource "ecloud_router" "test-router" {
+	vpc_id = ecloud_vpc.test-vpc.id
+	availability_zone_id = data.ecloud_availability_zone.test-az.id
+	name = "test-router"
+}
+
+resource "ecloud_network" "test-network" {
+	router_id = ecloud_router.test-router.id
+	name = "test-network"
+    subnet = "10.0.0.0/24"
+}
+
+resource "ecloud_instance" "test-instance-vcpu" {
+	vpc_id = ecloud_vpc.test-vpc.id
+	network_id = ecloud_network.test-network.id
+	name = "{{.Name}}"
+	image_id = data.ecloud_image.ubuntu2204.id
+	volume_capacity = 40
+	ram_capacity = 1024
+{{if gt .VCPUCores 0 }}
+	vcpu_cores = {{.VCPUCores}}
+{{end}}{{if or (gt .VCPUSockets 0) (gt .VCPUCoresPerSocket 0)}}
+	vcpu {
+		sockets = {{.VCPUSockets}}
+		cores_per_socket = {{.VCPUCoresPerSocket}}
+	}
+{{end}}
+}
+`))
+
+	buf := bytes.Buffer{}
+	err := tmpl.Execute(&buf, config)
+	if err != nil {
+		panic(err)
+	}
+
+	return buf.String()
 }
